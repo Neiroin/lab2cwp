31c3926788bc6c699f1de04fdafdc704baa0997d
Neiroin <plummy121@gmail.com>
2018-10-08 12:41:00 +0300

    com

---
+++ client.js
@@ -0,0 +1,53 @@
+
+const net = require('net');
+const fs = require('fs');
+const port = 8124;
+const initialString = 'QA';
+const ANSWER_IS_FALSE = 'DEC';
+const ANSWER_IS_TRUE = 'ACK';
+
+const client = new net.Socket();
+let currentIndex = -1;
+client.setEncoding('utf8');
+
+let questions = [];
+client.connect({port: port, host: '127.0.0.1'}, () => {
+    fs.readFile("qa.json", (err, text) => {
+        if (!err) {
+            questions = JSON.parse(text);
+            client.write(initialString);
+        }
+        else console.error(err);
+    });
+});
+
+client.on('data', (data) => {
+    if (data === ANSWER_IS_FALSE)
+        client.destroy();//уничтожит его файловый дескриптор
+    if (data === ANSWER_IS_TRUE)
+        sendQuestion();
+    else {
+        let qst = questions[currentIndex];
+        let answer = qst.good;
+        console.log('\n' + qst.quest);
+        console.log('Answer:' + data);
+        console.log('Server:' + answer);
+        console.log('Result:' + (data === answer ? 'It is a right answer': 'It is a false answer'));
+        sendQuestion();
+    }
+});
+
+client.on('close', function () {
+    console.log('Connection closed');
+});
+
+
+
+function sendQuestion() {
+    if (currentIndex < questions.length -1) {
+        let qst = questions[++currentIndex].quest;
+        client.write(qst);
+    }
+    else
+        client.destroy();
+}
---
+++ server.js
@@ -0,0 +1,49 @@
+//подкл модули на сервере
+const net = require('net');
+const fs = require('fs');
+const port = 8124;
+const initiateString = 'QA';// Для начала взаимодействия клиент отправляет строк
+const ANSWER_IS_RIGHT = 'ACK';//подтверждает
+const ANSWER_IS_FALSE = 'DEC';//отказывает
+const logger = fs.createWriteStream('client_id.log');
+
+let seed = 0;//уникальный ид
+
+const server = net.createServer((client) => {
+    console.log('Client connected');
+    client.setEncoding('utf8');
+
+    client.on('data', (data, err) =>
+    {
+        if (err) console.error(err);
+        else if (!err && data === initiateString)
+        {
+            //Каждому подключенному клиенту сервер должен присваивать уникальный идентификатор.
+            client.id = Date.now() + seed++;
+            writeLog('Client #' + client.id + ' connected\n');
+            client.write(data === initiateString ? ANSWER_IS_RIGHT : ANSWER_IS_FALSE);
+        }
+        else if (!err && data !== initiateString) {
+            writeLog('Client #' + client.id + ' has asked: ' + data + '\n');
+            let answer = generateAnswer();
+            writeLog('Server answered to Client #' + client.id + ': ' + answer + '\n');
+            client.write(answer);
+        }
+    });
+    client.on('end', () =>
+    {
+        logger.write('Client #'+ client.id+ ' disconnected');
+        console.log('Client disconnected')
+    });
+});
+function writeLog(data)
+{
+    logger.write(data);
+}
+function generateAnswer()
+{
+    return Math.random() > 0.5 ? '1' : '0';
+}
+server.listen(port, () => { //прослушиваем порт
+    console.log(`Server listening on localhost: ${port}`);
+});
